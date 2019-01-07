aba1d22c91cfc70ac621c5c0c99a79a672232564
fesoa <fernandofesoa@gmail.com>
2019-01-08 01:34:25 +0900

    +

--- Section4/4_1_1.py
+++ Section4/4_1_1.py
@@ -16,7 +16,7 @@ data3 = {"Apple","Mandarin","Orange"}
 
 # 바이너리 쓰기
 with open(bfilename, 'wb') as f: #binary로 쓰겠다
-    pickle.dump(data1,f) # dumps: 객체를 바이너리로 쓸 때/ dumps: 문자열 직렬화
+    pickle.dump(data1,f) # dumps: 객체를 직렬화/ dumps: 문자열 직렬화
     pickle.dump(data2,f)
     pickle.dump(data3,f)
 
@@ -27,3 +27,17 @@ with open(tfilename, 'wt') as f:
     f.write(data2)
     f.write('\n')
     f.writelines('\n'.join(data3))
+
+# 바이너리 읽기
+with open(bfilename, 'rb') as f:    # 바이너리로 읽으면 객체를 객체 그대로 읽어올 수 있음
+    b = pickle.load(f) #loads: 문자열 역직렬화
+    print(type(b), 'Binary Read1 |', b)
+    b = pickle.load(f)
+    print(type(b), 'Binary Read2 |', b)
+    b = pickle.load(f)
+    print(type(b), 'Binary Read3 |', b)
+
+# 텍스트 읽기
+with open(tfilename, 'rt') as f:
+    for i, line in enumerate(f,1):
+        print(type(line),'Text Read' + str(i) + ' | ', line, end='')
